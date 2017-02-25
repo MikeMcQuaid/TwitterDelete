8fc456d8ce7d8a15cea29c8c09ee639609cc5556
Olly Headey <olly@freeagent.com>
2017-02-25 21:33:24 +0000

    Declare tweets_not_found in correct place

--- twitter_delete.rb
+++ twitter_delete.rb
@@ -116,6 +116,7 @@ if !@options[:force]
 end
 
 puts "==> Unfavoriting #{tweets_to_unfavourite.size} tweets"
+tweets_not_found = []
 tweets_to_unfavourite.each_slice(MAX_TWEETS_PER_REQUEST) do |tweets|
   begin
     api_call :unfavorite, tweets
@@ -125,7 +126,6 @@ tweets_to_unfavourite.each_slice(MAX_TWEETS_PER_REQUEST) do |tweets|
 end
 
 puts "==> Deleting #{tweets_to_delete.size} tweets"
-tweets_not_found = []
 tweets_to_delete.each_slice(MAX_TWEETS_PER_REQUEST) do |tweets|
   begin
     api_call :destroy_status, tweets
